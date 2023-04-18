04fdf755031f072cd7c6ab9e139391047b23c3c5
Owen Z <16374972+Apollogeist@users.noreply.github.com>
2023-04-18 10:22:29 -0500

    Fix circular import causing error

--- TAScheduler/models/__init__.py
+++ TAScheduler/models/__init__.py
@@ -1,3 +1,3 @@
+from .model_section import Section
 from .model_user import User
 from .model_course import Course
-from .model_section import Section
--- TAScheduler/models/model_section.py
+++ TAScheduler/models/model_section.py
@@ -1,9 +1,8 @@
 from django.db import models
-from TAScheduler.models import Course
 
 class Section(models.Model):
     name = models.CharField(max_length=128)
     location = models.CharField(max_length=128)
     time = models.CharField(max_length=256)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='sections')
+    course = models.ForeignKey("TAScheduler.Course", on_delete=models.CASCADE, related_name='sections')
     tas = models.ManyToManyField("TAScheduler.User", related_name='sections')
