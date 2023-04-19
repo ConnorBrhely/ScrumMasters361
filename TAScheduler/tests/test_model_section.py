9cb45744a3de6e0b0b630464758c914310fcaea7
Zoya Ammar <zoyaammar97@gmail.com>
2023-04-19 18:46:37 -0500

    I added test cases to test_model_section.py.

--- TAScheduler/tests/test_model_section.py
+++ TAScheduler/tests/test_model_section.py
@@ -22,3 +22,25 @@ class TestModelUser(TestCase):
             )
 
         # TODO: Add tests for Section model
+
+
+
+    def test_update_name(self):
+        new_name = 'New Section Name'
+        self.section.update_name(new_name)
+        self.assertEqual(self.section.name, new_name)
+
+    def test_add_ta(self):
+        self.section.add_ta(self.user)
+        self.assertEqual(self.section.tas.first(), self.user)
+    def test_add_non_ta_user_as_ta(self):
+        with self.assertRaises(ValueError):
+            self.section.add_ta(User.objects.create(
+                name='Test Student',
+                type='STUDENT',
+            ))
+
+    def test_remove_ta(self):
+        self.section.tas.add(self.user)
+        self.section.remove_ta(self.user)
+        self.assertFalse(self.section.tas.exists())
