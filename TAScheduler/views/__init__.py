0ab4786eca7057753f0d11b0a97b5435f9747446
bencrosby1 <bwcrosby@uwm.edu>
2023-05-03 17:57:38 -0500

    added accounts to init

--- TAScheduler/views/__init__.py
+++ TAScheduler/views/__init__.py
@@ -4,3 +4,4 @@ from .view_login import Login
 from .view_logout import Logout
 from .view_create_section import CreateSection
 from .view_create_course import CreateCourse
+from .view_accounts import Accounts
