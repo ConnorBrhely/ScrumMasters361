565cbb05b7c31ad981b3c1b7806474e2f39fef77
Zoya Ammar <zoyaammar97@gmail.com>
2023-05-09 17:47:11 -0500

    I tried to fix my html page and the modify_account view.

--- TAScheduler/views/view_modify_account.py
+++ TAScheduler/views/view_modify_account.py
@@ -1,25 +1,19 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from .forms import UserUpdateForm, ProfileUpdateForm
+from TAScheduler.models import UserAccount
 
 @login_required
-def modify_account(request):
+def home_view(request):
+    account = UserAccount.objects.get(user=request.user)
+    message = None
+    status = None
+
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f'Your account has been updated!')
-            return redirect('modify_account')
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
+        # Handle any post requests here
+        pass
 
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
-
-    return render(request, 'modifyaccount.html', context)
+    return render(request, 'home.html', {
+        'account': account,
+        'message': message,
+        'status': status
+    })
