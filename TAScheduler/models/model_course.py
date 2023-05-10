6963bc18cc9710bcb3534a9b55230d1f36a6081f
ConnorBrhely <clbrhely@uwm.edu>
2023-05-09 19:09:12 -0500

    Model Course stuff

--- TAScheduler/models/model_course.py
+++ TAScheduler/models/model_course.py
@@ -12,3 +12,22 @@ class Course(models.Model):
     term_season = models.CharField(max_length=6, choices=TERM_SEASON_CHOICES)
     # String must be used instead of a foreign key to avoid circular imports
     instructor = models.ForeignKey("TAScheduler.UserAccount", null=True, on_delete=models.SET_NULL, related_name='instructor')
+
+    def add_instructor(self, user):
+        from TAScheduler.models import UserAccount
+        if user.type != UserAccount.UserType.TA:
+            raise ValueError('User must be a instructor')
+        self.instructor.add(user)
+        self.save()
+        return self
+
+    def update_name(self, name):
+        self.name = name
+        self.save()
+        return self
+
+    def set_term(self, term_season, term_year):
+        self.term_season = term_season
+        self.term_year = term_year
+        self.save()
+        return self
