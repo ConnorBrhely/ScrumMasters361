a6feef6b9492c489eb0ff0925f9567d5adc2fc9c
SleepyLink1 <88170820+SleepyLink1@users.noreply.github.com>
2023-05-17 20:33:23 -0500

    UPDATED EDITCOURSE VIEW

--- TAScheduler/views/view_edit_course.py
+++ TAScheduler/views/view_edit_course.py
@@ -14,18 +14,18 @@ class EditCourse(View):
         return self.render_simple1(request)
 
     def post(self, request):
-        course = request.GET["coursename"]
-        name=request.POST["name"].strip()
-        number=request.POST["number"].strip()
-        term_year = request.POST["term_season"].strip()
-        term_season = request.POST["season"]
-        # s = s.split()
-        # term_season = s[0]
-        # term_year = s[1]
-        s = request.POST["instructor"].strip
-        if s is not none:
-            instructor = UserAccount.objects.get(pk=int(s))
-        if name == "" or number == "" or term_season == "" or term_year == "":
+        # course = request.GET["coursename"]
+        name = request.POST["coursename"].strip()
+        number = request.POST["coursenumber"].strip()
+        term_year = request.POST["courseterm"].strip()
+        term_season = request.POST["term_season"]
+        instructor = request.POST["courseinstructor"].strip()
+        if instructor != "none":
+            instructor = UserAccount.objects.get(pk=int(instructor))
+        else:
+            instructor = None
+
+        if name == "" or number == "" or term_season == "" or term_year == "" or instructor == "":
             return self.render_simple1(request, "One or more blank field detected", "error")
         course_id = request.POST["course_id"]
         course_to_edit = Course.objects.get(pk=course_id)
@@ -33,8 +33,7 @@ class EditCourse(View):
         course_to_edit.update_number(number)
         course_to_edit.update_term_year=(term_year)
         course_to_edit.update_term_season=(term_season)
-        if s is not none:
-            course_to_edit.update_instructor=(instructor)
+        course_to_edit.update_instructor = (instructor)
 
 
     def render_simple1(self, request, message="", status="success"):
@@ -45,5 +44,7 @@ class EditCourse(View):
             "status": status,
             "account": UserAccount.objects.get(user_id=request.user.id),
             "editcourse": course_to_edit,
+            "instructors": UserAccount.objects.filter(type=UserAccount.UserType.INSTRUCTOR)
+
 
         })
