c52713b3878ab8421fc0ef3a792c3fe4f7fbbac5
Daniel <88170820+SleepingLink1@users.noreply.github.com>
2023-05-18 06:27:09 -0500

    added return statement

--- TAScheduler/tests/unit_tests/test_model_course.py
+++ TAScheduler/tests/unit_tests/test_model_course.py
@@ -24,3 +24,33 @@ class TestModelCourse(TestCase):
             time='Test Time',
             course=self.course,
         )
+
+    def test_update_name(self):
+        self.course.update_name("hello")
+        self.assertEqual(self.course.name, "hello", msg="Course name not updated")
+    def test_update_no_name(self):
+        self.course.update_name("")
+        self.assertRaises(ValueError, msg="Course Name must not be empty")
+
+    def test_update_invalid_name(self):
+        self.course.update_name("111")
+        self.assertRaises(ValueError, msg="Course Name must contain letters")
+
+    def test_update_name_whitespace(self):
+       with self.assertRaises(ValueError, msg="Course Name did not raise ValueError for only whitespace"):
+           self.course.update_name("   \t\n")
+
+    def test_update_number(self):
+        self.course.update_name("123")
+        self.assertEqual(self.course.name, "hello", msg="Course Number not updated")
+    def test_update_no_number(self):
+        self.course.update_name("")
+        self.assertRaises(ValueError, msg="Course Number must not be empty")
+
+    def test_update_invalid_number(self):
+        self.course.update_number("abcd")
+        self.assertRaises(ValueError, msg="Course Number must contain numbers")
+
+    def test_update_number_whitespace(self):
+       with self.assertRaises(ValueError, msg="Course Number did not raise ValueError for only whitespace"):
+           self.course.update_number("   \t\n")
