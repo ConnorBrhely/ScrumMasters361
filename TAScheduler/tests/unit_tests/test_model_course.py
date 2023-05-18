9c9a8102fb16dcba5df08c8d15bb3dda32ad1140
Daniel <88170820+SleepingLink1@users.noreply.github.com>
2023-05-18 06:35:16 -0500

    added return statement

--- TAScheduler/tests/unit_tests/test_model_course.py
+++ TAScheduler/tests/unit_tests/test_model_course.py
@@ -37,14 +37,15 @@ class TestModelCourse(TestCase):
         self.assertRaises(ValueError, msg="Course Name must contain letters")
 
     def test_update_name_whitespace(self):
-       with self.assertRaises(ValueError, msg="Course Name did not raise ValueError for only whitespace"):
-           self.course.update_name("   \t\n")
+      self.course.update_name("   \t\n")
+      self.assertRaises(ValueError, msg="Course Name did not raise ValueError for only whitespace")
+
 
     def test_update_number(self):
-        self.course.update_name("123")
-        self.assertEqual(self.course.name, "hello", msg="Course Number not updated")
+        self.course.update_number("123")
+        self.assertEqual(self.course.number, "hello", msg="Course Number not updated")
     def test_update_no_number(self):
-        self.course.update_name("")
+        self.course.update_number("")
         self.assertRaises(ValueError, msg="Course Number must not be empty")
 
     def test_update_invalid_number(self):
@@ -52,5 +53,24 @@ class TestModelCourse(TestCase):
         self.assertRaises(ValueError, msg="Course Number must contain numbers")
 
     def test_update_number_whitespace(self):
-       with self.assertRaises(ValueError, msg="Course Number did not raise ValueError for only whitespace"):
-           self.course.update_number("   \t\n")
+       self.course.update_number("   \t\n")
+       self.assertRaises(ValueError, msg="Course Number did not raise ValueError for only whitespace")
+
+
+    def test_update_term_year(self):
+        self.course.update_term_year("2025")
+        self.assertEqual(self.course.term_year, "2025", msg="Term Year not updated")
+
+    def test_empty_term_year(self):
+        self.course.update_term_year(None)
+        self.assertRaises(ValueError, msg="Course Term must not be None")
+
+
+
+    def test_update_invalid_term_year(self):
+        self.course.update_number("abcd")
+        self.assertRaises(ValueError, msg="Course Term must contain numbers")
+
+    def test_update_number_whitespace(self):
+        self.course.update_term_year("   \t\n")
+        self.assertRaises(ValueError, msg="Course Term did not raise ValueError for only whitespace")
